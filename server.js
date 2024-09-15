3cc024aa091869fa5cc3665305ba972a3376696d
hightree <dev.hightree@gmail.com>
2024-09-15 16:16:59 +0900

    머쓱, console.log를 지우지 않아서 지웠다.
    fileURLToPath를 사용하면 어떻게 되는지
    dirname을 사용하면 어떻게 되는지 알고 싶어서
    console.log로 찍어봤다.

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ import { fileURLToPath } from 'url';
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     const __filename = fileURLToPath(import.meta.url);
-    console.log(__filename);
     const __dirname = path.dirname(__filename);
-    console.log(__dirname);
     const filePath = path.join(__dirname, 'index.html');
     fs.readFile(filePath, 'utf8', (err, html) => {
       res.writeHead(200, { 'Content-Type': 'text/html' });
