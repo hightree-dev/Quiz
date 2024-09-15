571953bc34aef9d203d3192838b5045f6a05ed28
hightree <dev.hightree@gmail.com>
2024-09-15 16:15:21 +0900

    서버에서 html 파일 전송!
    서버에서 html 파일을 불러올 때, 현재 경로가 필요하다.
    CommonJS의 경우에는 __dirname을 제공하고 있어서 바로 사용할 수 있지만, ESM의 경우에는 그러하지 않다.
    그래서 ESM에서는 import.meta.url로 현재 모듈(js 파일)의 URL을 받아올 수 있다. fileURLToPath를 통해 URL을 파일 경로로 변환할 수 있고, dirname을 통해 파일 이름을 제거한 디렉터리 경로(폴더 경로)를 얻을 수 있다.
    path.join으로 index.html의 경로를 만들고 fs.readFile을 통해 index.html을 불러올 수 있다.

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import http from 'node:http';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify({
-    data: 'Hello World!',
-  }));
+  if (req.url === '/') {
+    const __filename = fileURLToPath(import.meta.url);
+    console.log(__filename);
+    const __dirname = path.dirname(__filename);
+    console.log(__dirname);
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, 'utf8', (err, html) => {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(html);
+    });    
+  }
 });
 
 const port = 8000;
