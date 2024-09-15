905b74b0caf847b1890a71682b6e6c4dabc93f2e
hightree <dev.hightree@gmail.com>
2024-09-15 23:52:18 +0900

    각종 API 작업과 DB 작업
    200줄도 안되는 짧은 코드인데 쉽지 않았다.
    가장 불편한건 코드에 중복된게 너무 많고 더럽다는 것. 그리고 에러 처리가 잘 안 되어있는 것.
    익숙하지 않아서 쉽지 않았던 것 같기도 하다. 익숙해지면 이런 작업도 쉽게 쉽게하겠지?

--- server.js
+++ server.js
@@ -2,34 +2,108 @@ import http from 'node:http';
 import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
+import sqlite3 from 'sqlite3';
+
+sqlite3.verbose();
+const db = new sqlite3.Database('./quiz.db');
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
     const filePath = path.join(__dirname, 'index.html');
     fs.readFile(filePath, 'utf8', (err, html) => {
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(html);
-    });    
-  } else if (req.url === '/data') {
-    if (req.method === 'GET') {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ data: 'Data' }));
-    } else if (req.method === 'POST') {
+    });
+  } else if (req.url === '/quiz') {
+    if (req.method === 'POST') {
       let body = '';
       req.on('data', chunk => { body += chunk; });
       req.on('end', () => {
-        console.log('Received POST data', body);
-        try {
-          const parsedData = JSON.parse(body);
-          console.log('Parsed data: ', parsedData);
-        } catch(e) {
-          console.error('Failed to parse JSON: ', e);
+        const quiz = JSON.parse(body);
+
+        const { question, answer } = quiz;
+
+        if (question && answer) {
+          db.run('INSERT INTO quiz (question, answer) VALUES (?, ?)', [question, answer], function (err) {
+            if (err) {
+              res.writeHead(500, { 'Content-Type': 'text/plain' });
+              res.end('Failed to add quiz');
+              console.error(err);
+            } else {
+              console.log(this);
+              res.writeHead(201, { 'Content-Type': 'application/json' });
+              res.end(JSON.stringify({ id: this.lastID }));
+            }
+          });
+        } else {
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end('Invalid quiz data');
         }
-        res.writeHead(201, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Data received successfully' }));
       });
+    }
+  } else if (req.url === '/quizzes') {
+    if (req.method === 'GET') {
+      db.all('SELECT * FROM quiz', (err, rows) => {
+        if (err) {
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Internal Server Error');
+          console.error(err);
+          return;
+        }
+        if (rows.length > 0) {
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(rows));
+        } else {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end('No quizzes found');
+        }
+      });
+    }
+  } else if (req.url.startsWith('/quiz/')) {
+    const quizId = parseInt(req.url.split('/')[2]);
+    if (req.method == 'GET') {
+      if (quizId === -1) {
+        db.get('SELECT * FROM quiz ORDER BY id DESC LIMIT 1', (err, row) => {
+          if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+            console.error(err);
+            return;
+          }
+          if (row) {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(row));
+          } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Quiz not found');
+          }
+        });
+      }
+      else if (quizId){
+        db.get('SELECT * FROM quiz WHERE id = ?', [quizId], (err, row) => {
+          if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Internal Server Error');
+            console.error(err);
+            return;
+          }
+          if (row) {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(row));
+          } else {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Quiz not found');
+          }
+        });
+      }
+      else {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Invalid quizId');
+      }
     }
   }
 });
